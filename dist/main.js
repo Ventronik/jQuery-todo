01a30a0298bb9cc50cdde1fce8ad16eaf5f97c0d
Dustin Ruskell <dustin.ruskell@avanade.com>
2023-12-15 15:55:21 -0800

    implemented basic to do list

--- dist/main.js
+++ dist/main.js
@@ -1,24 +1,63 @@
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
+"use strict";
+class TodoApp extends HTMLElement {
+    constructor() {
+        super();
+        this.todoList = [];
+        this.attachShadow({ mode: 'open' });
+        // Define the HTML structure within the shadow DOM
+        this.shadowRoot.innerHTML = `
+        <style>
+          /* Add any styling for your component here */
+        </style>
+        <div id="app">
+          <input type="text" class="new-item" placeholder="Add a new item">
+          <button class="add-button">Add</button>
+          <ul class="todo-list"></ul>
+          <div class="item-count"></div>
+        </div>
+      `;
     }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "jquery", "./index.ts"], factory);
+    connectedCallback() {
+        // Add event listeners or any other initialization logic here
+        const addButton = this.shadowRoot.querySelector('.add-button');
+        if (addButton) {
+            addButton.addEventListener('click', this.addItem.bind(this));
+        }
+        // You can add more event listeners as needed
+        this.render();
     }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var jquery_1 = __importDefault(require("jquery"));
-    require("./index.ts");
-    // DOM Ready
-    (0, jquery_1.default)(function () {
-        var $container = (0, jquery_1.default)('#app');
-        $container.append('<input type="text" class="new-item" placeholder="Add a new item">');
-        $container.append('<button class="add-button">Add</button>');
-        $container.append('<div class="item-count"></div>');
-    });
-});
+    addItem() {
+        const input = this.shadowRoot.querySelector('.new-item');
+        const newItemText = input.value.trim();
+        if (newItemText !== '') {
+            this.todoList.push({ text: newItemText, completed: false });
+            input.value = '';
+            this.render();
+        }
+    }
+    render() {
+        const todoListContainer = this.shadowRoot.querySelector('.todo-list');
+        const itemCount = this.shadowRoot.querySelector('.item-count');
+        // Clear previous content
+        if (todoListContainer) {
+            todoListContainer.innerHTML = '';
+            // Render todo items
+            this.todoList.forEach((item, index) => {
+                const listItem = document.createElement('li');
+                listItem.innerHTML = `
+            <input type="checkbox" class="toggle" data-index="${index}" ${item.completed ? 'checked' : ''}>
+            <span>${item.text}</span>
+          `;
+                if (todoListContainer) {
+                    todoListContainer.appendChild(listItem);
+                }
+            });
+            // Update item count
+            if (itemCount) {
+                itemCount.textContent = `Total items: ${this.todoList.length}`;
+            }
+        }
+    }
+}
+// Define the custom element
+customElements.define('todo-app', TodoApp);
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,80 @@
-import $ from 'jquery';
-import './index.ts';
+interface TodoItem {
+    text: string;
+    completed: boolean;
+  }
+  
+  class TodoApp extends HTMLElement {
+    private todoList: TodoItem[] = [];
+  
+    constructor() {
+      super();
+  
+      this.attachShadow({ mode: 'open' });
+  
+      // Define the HTML structure within the shadow DOM
+      this.shadowRoot!.innerHTML = `
+        <style>
+          /* Add any styling for your component here */
+        </style>
+        <div id="app">
+          <input type="text" class="new-item" placeholder="Add a new item">
+          <button class="add-button">Add</button>
+          <ul class="todo-list"></ul>
+          <div class="item-count"></div>
+        </div>
+      `;
+    }
+  
+    connectedCallback() {
+      // Add event listeners or any other initialization logic here
+      const addButton = this.shadowRoot!.querySelector('.add-button');
+      if (addButton) {
+        addButton.addEventListener('click', this.addItem.bind(this));
+      }
+      
+      // You can add more event listeners as needed
+      this.render();
+    }
+  
+    addItem() {
+      const input = this.shadowRoot!.querySelector('.new-item') as HTMLInputElement;
+      const newItemText = input.value.trim();
+      if (newItemText !== '') {
+        this.todoList.push({ text: newItemText, completed: false });
+        input.value = '';
+        this.render();
+      }
+    }
+  
+    render() {
+      const todoListContainer = this.shadowRoot!.querySelector('.todo-list') as HTMLUListElement;
+      const itemCount = this.shadowRoot!.querySelector('.item-count') as HTMLDivElement;
+  
+      // Clear previous content
+      if (todoListContainer) {
+        todoListContainer.innerHTML = '';
+  
+        // Render todo items
+        this.todoList.forEach((item, index) => {
+          const listItem = document.createElement('li');
+          listItem.innerHTML = `
+            <input type="checkbox" class="toggle" data-index="${index}" ${item.completed ? 'checked' : ''}>
+            <span>${item.text}</span>
+          `;
+          if (todoListContainer) {
+            todoListContainer.appendChild(listItem);
+          }
+        });
+  
+        // Update item count
+        if (itemCount) {
+          itemCount.textContent = `Total items: ${this.todoList.length}`;
+        }
+      }
+    }
+  }
+  
+  // Define the custom element
+  customElements.define('todo-app', TodoApp);
 
-// DOM Ready
-$(function() {
-  const $container = $('#app');
-  $container.append('<input type="text" class="new-item" placeholder="Add a new item">');
-  $container.append('<button class="add-button">Add</button>');
-  $container.append('<div class="item-count"></div>');
-});
+  
